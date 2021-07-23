226344ad3c66943ce2673d2393159e017ba0add1
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-23 15:07:12 +0200

    running bash in yml

--- .github/workflows/test.sh
+++
@@ -1,8 +0,0 @@
-
-deploy == [$curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
-
-while p -p  [deploy] > null;
-  do sleep 1;
-  done;
-  echo "deploy"
- 
