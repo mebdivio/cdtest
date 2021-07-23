4758fdd495cc71498318ce8922293657e737c00a
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-23 15:17:13 +0200

    running bash in yml

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-deploy == [$curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
+deploy = [$curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
 
 while p -p  [deploy] > null;
   do sleep 1;
--- testlive.sh
+++ testlive.sh
@@ -1,5 +1,5 @@
 
-deploy == [$curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
+deploy = [$curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
 
 while p -p  [deploy] > null;
   do sleep 1;
