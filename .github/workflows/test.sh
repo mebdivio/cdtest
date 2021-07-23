6acb1a4915164bcb4f5a7f1e15961b54bc2a3ccf
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-23 14:11:36 +0200

    Testing the bash file

---
+++ .github/workflows/test.sh
@@ -0,0 +1,8 @@
+
+deploy == [$curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
+
+while p -p  [deploy] > null;
+  do sleep 1;
+  done;
+  echo "deploy"
+ 
