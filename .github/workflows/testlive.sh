8252f0672075dcb714b580cba1393768ffd3e5e7
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-23 14:19:05 +0200

    Testing the testlive.sh

---
+++ .github/workflows/testlive.sh
@@ -0,0 +1,8 @@
+
+deploy == [$curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN"]
+
+while p -p  [deploy] > null;
+  do sleep 1;
+  done;
+  echo "deploy"
+ 
