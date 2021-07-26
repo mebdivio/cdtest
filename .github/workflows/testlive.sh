6580f7c3f36feeae44c2f078adf0e65de3e33b2f
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-26 09:48:02 +0200

    Testing Live Deployment

--- .github/workflows/testlive.sh
+++ .github/workflows/testlive.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-DEPLOY = $($curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")
+DEPLOY = $(curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")
 echo "Deployment started"
-while p -p  [$DEPLOY]
-  do sleep 1;
+echo $DEPLOY
+while [$DEPLOY]; do 
+  sleep 1
+  echo $DEPLOY
+  DEPLOY = $(curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")
 done;
 echo "Deployment Completed with $DEPLOY"
  
