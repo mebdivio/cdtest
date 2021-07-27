3be230640baeb649d06c8b36f63f442d21921be2
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-27 11:24:41 +0200

    Added script for deployment status check

--- .github/workflows/testlive.sh
+++ .github/workflows/testlive.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
-DEPLOY = $(curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")
-echo "Deployment started"
-echo $DEPLOY
-while [$DEPLOY]; do 
+while true; do 
   sleep 1
-  echo $DEPLOY
-  DEPLOY = $(curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")
-done;
-echo "Deployment Completed with $DEPLOY"
- 
+  DEPLOY="$(curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")"
+  echo "${DEPLOY}" | jq '.' > deploy.json
+  status="$(jq '.status' deploy.json)"
+  echo "${status} on progress"
+  success="$(jq '.success' deploy.json)"
+  echo "Success is still ${success}"
+
+  if [ $success=='true' ] || [ $success=='false' ]; then
+    if [ $success == 'true' ]; then 
+        echo "Deployment has completed successfully"
+        exit 0
+    else 
+        echo "Deplyment has failed"
+        exit 1
+    fi
+    break
+  fi
+
+done
